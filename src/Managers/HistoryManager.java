ae569e75bee0df764952eed750995c0791d08605
pupil <pupil@ivkhk.ee>
2023-10-13 15:26:05 +0300

    add manager book, reader and history

---
+++ src/Managers/HistoryManager.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Managers;
+
+import entity.Book;
+import entity.History;
+import entity.Reader;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class HistoryManager {
+    private Scanner scanner;
+    public HistoryManager(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    public History giveOutBook(Book[] books, Reader[] readers){
+        History history = new History();
+        /*
+            1.вывести список читателей
+            2. попросить пользователя выбрать номер читателей из списка
+            3.добавить выбранного читателя из массива reader в history
+            4. сделать 1-3 пункт для книги
+            5.добавить в history дату выдачи книги (текущую дату)
+        */
+        System.out.println("List readers");
+        for (int i = 0; i < readers.length; i++) {
+            System.out.printf("%d. %s %s %s%n",i+1,readers[i].getFirstname(),readers[i].getLastname(), readers[i].getPhone());
+        }
+        int selectedReaderNumber = scanner.nextInt(); scanner.nextLine();
+        for (int i = 0; i < books.length; i++) {
+            System.out.printf("%d. %s %s %s%n",i+1,books[i].getTitle(),
+                    books[i].getPublishedYear(),Arrays.toString(books[i].getAuthors()));
+        }
+        int selectedBookNumber = scanner.nextInt(); scanner.nextLine();
+        history.setBook(books[selectedBookNumber-1]);
+        history.setDateOnHand(new GregorianCalendar().getTime());
+        return history;
+    }
+    
+}
--- src/jktv22library/App.java
+++ src/jktv22library/App.java
@@ -4,8 +4,15 @@
  */
 package jktv22library;
 
+import Managers.HistoryManager;
+import Managers.ReaderManager;
+import Managers.BookManager;
 import entity.Author;
 import entity.Book;
+import entity.History;
+import entity.Reader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -13,7 +20,9 @@ import java.util.Scanner;
  * @author pupil
  */
 class App {
-
+private Book[] books = new Book[0];
+private Reader[] readers = new Reader[0];
+private History[] histories = new History[0];
     void run() {
         boolean repeat = true;
         Scanner scanner = new Scanner(System.in);
@@ -21,6 +30,8 @@ class App {
             System.out.println("Select task: ");
             System.out.println("0. Exit");
             System.out.println("1.Add new book");
+            System.out.println("2.Add new reader");
+            System.out.println("3.Give out a book to read");
             System.out.print("Set task: ");
             int task = scanner.nextInt();scanner.nextLine();            
             switch (task) {
@@ -29,32 +40,34 @@ class App {
                     repeat = false;
                     break;
                 case 1:
-                    Book book = new Book();
-                    System.out.print("Input title: ");
-                    book.setTitle(scanner.nextLine());
-                    System.out.println("Enter published year: ");
-                    book.setPublishedYear(scanner.nextInt());
-                    scanner.nextLine();
-                    System.out.println("How many authors: ");
-                    int countAuthors= scanner.nextInt(); scanner.nextLine();
-                    for (int i = 0; i < countAuthors; i++) {
-                        System.out.println(i+1+"author: ");
-                         System.out.println("Author firstname: ");
-                         String authorFirstname = scanner.nextLine();
-                         System.out.println("Author lastname: ");
-                         String authorLastname = scanner.nextLine();
-                         book.addAuthor(new Author(authorFirstname,authorLastname));
-                    }
-                    System.out.println("Added book: ");
-                    System.out.println(book.toString());
+                    BookManager bookManager = new BookManager(scanner);
+                    addBookToArray(bookManager.addBook());                   
                     break;
                 case 2:
-                    
+                    ReaderManager readerManager = new ReaderManager(scanner);
+                    addReaderToArray(readerManager.addReader());
+                    break;
+                case 3:
+                   HistoryManager historyManager = new HistoryManager(scanner);
+                   addHistoryToArray(historyManager.giveOutBook(books, readers));
                     break;
                 default:
                     System.out.println("Choice number from list !");;
             }
         } while (repeat);
     }
-    
+
+    private void addBookToArray(Book book) {
+        this.books = Arrays.copyOf(books, books.length + 1);
+        this.books[books.length -1 ] = book;
+    }
+
+    private void addReaderToArray(Reader reader) {
+        this.readers = Arrays.copyOf(readers, readers.length + 1);
+        this.readers[readers.length -1 ] = reader;
+    }
+    private void addHistoryToArray(History history) {
+        this.histories = Arrays.copyOf(histories, histories.length + 1);
+        this.histories[histories.length -1 ] = history;
+    }
 }
